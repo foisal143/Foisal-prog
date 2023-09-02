110b82b859d03f31df21f36eff96eb151c623845
Md foisal islam~ <islammdfoisal54@gmail.com>
2023-09-02 07:07:21 -0700

    portfolio

---
+++ js/hello.js
@@ -0,0 +1,32 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      'submit',
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+})();
+// form validation end
+// modal for hire me start
+const myModal = document.getElementById('myModal');
+const myInput = document.getElementById('myInput');
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus();
+});
+// modal end
